b9f9aa581bf81aa4b24177d006f20e591a19e1aa
Thomas A Caswell <tcaswell@bnl.gov>
2025-05-25 00:22:15 -0400

    ENH: add UI elements to mask demo

--- demos/mask.py
+++ demos/mask.py
@@ -1,24 +1,30 @@
 # %%
 from pathlib import Path
-from typing import TypeVar, cast
 
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
+from matplotlib.image import AxesImage
 from matplotlib.patches import Rectangle
+from matplotlib.widgets import Slider
 
 from multihead.file_io import RawHRPD11BM
 from multihead.raw_proc import CrystalROI, find_crystal_range
 
-dt = TypeVar("dt", bound=np.generic, covariant=True)
 # %%
 
-root = Path("/mnt/scratch/hrd/data_cache/Lab6_testdata_share/data/")
-f1 = "test_04_15_2025_0009"
-f2 = "LaB6_WoSlits_04_30_0000"
-f3 = "AL2O3_WoSlits_04_30_0000"
+root = Path("/mnt/scratch/hrd/data_cache/Lab6_testdata_share")
+data_root = root / "data"
+calib_root = root / "calib"
 
-t = RawHRPD11BM.from_root(root / f2)
+# f = "test_04_15_2025_0009"
+# f = "LaB6_WoSlits_04_30_0000"
+f = "AL2O3_WoSlits_04_30_0000"
+
+t = RawHRPD11BM.from_root(data_root / f)
+
+opening_radius = 7
+closing_radius = 15
 
 # %%
 sums = t.get_detector_sums()
@@ -27,208 +33,118 @@ sums = t.get_detector_sums()
 
 rois = {}
 thresholds = [0, 1, 2, 3, 5]
-fig = plt.figure(layout="compressed")
-figs = fig.subfigures(len(sums))
-for sfig, (k, v) in zip(figs, sums.items(), strict=True):
-    ax_arr = sfig.subplots(1, len(thresholds) + 1, sharex=True, sharey=True)
-    ax_orig, *ax_masked = ax_arr
-    # ax_orig.set_title("Detector sum")
-    ax_orig.axis("off")
-    ax_orig.imshow(v, vmax=max(1, float(np.percentile(v, 90))))
-    for th, ax in zip(thresholds, ax_masked, strict=True):
-        mask, croi = find_crystal_range(v > th, closing_radius=10, opening_radius=10)
-        ax.imshow(mask, cmap="gray")
-        ax.add_artist(
-            Rectangle(
-                (croi.cslc.start, croi.rslc.start),
-                croi.cslc.stop - croi.cslc.start,
-                croi.rslc.stop - croi.rslc.start,
-                facecolor="none",
-                edgecolor="r",
+fig = plt.figure(figsize=(15, 9), layout="compressed")
+fig.suptitle(f"{f}")
+data_fig, input_fig = fig.subfigures(2, height_ratios=[7, 1])
+
+
+def make_figure(fig, sums, opening_radius, closing_radius):
+    figs = fig.subfigures(1, len(sums) + 1, width_ratios=(0.5,) + (1,) * len(sums))
+    images: dict[tuple[int, int], AxesImage] = {}
+    rects: dict[tuple[int, int], Rectangle] = {}
+    for sfig, (k, v) in zip(figs[1:], sums.items(), strict=True):
+        ax_arr = sfig.subplots(len(thresholds) + 1, sharex=True, sharey=True)
+        ax_orig, *ax_masked = ax_arr
+        # ax_orig.set_title("Detector sum")
+        ax_orig.axis("off")
+        ax_orig.imshow(v, vmax=max(1, float(np.percentile(v, 90))))
+        ax_orig.set_title(f"Crystal {k}")
+        for th, ax in zip(thresholds, ax_masked, strict=True):
+            mask, croi = find_crystal_range(
+                v > th, closing_radius=closing_radius, opening_radius=opening_radius
             )
+            images[(k, th)] = ax.imshow(mask, cmap="gray")
+            rects[(k, th)] = ax.add_artist(
+                Rectangle(
+                    (croi.cslc.start, croi.rslc.start),
+                    croi.cslc.stop - croi.cslc.start,
+                    croi.rslc.stop - croi.rslc.start,
+                    facecolor="none",
+                    edgecolor="r",
+                )
+            )
+            ax.axis("off")
+            if sfig is figs[0]:
+                txt.set_in_layout(False)
+            rois[(k, th)] = croi
+
+    text_axes = figs[0].subplots(len(thresholds) + 1, sharex=True, sharey=True)
+    [ax.axis("off") for ax in text_axes]
+    text_axes[0].annotate(
+        "raw",
+        (0.5, 0.5),
+        xycoords="axes fraction",
+        rotation="vertical",
+        va="center",
+        ha="center",
+    )
+    for ax, th in zip(text_axes[1:], thresholds):
+        ax.annotate(
+            f"{th=}",
+            (0.5, 0.5),
+            xycoords="axes fraction",
+            rotation="vertical",
+            va="center",
+            ha="center",
         )
-        ax.axis("off")
-        # ax.set_title(f"threshold > {th}")
-        rois[(k, th)] = croi
+    return figs, images, rects
 
 
-# %%
-def compute_rois(
-    sums: dict[int, npt.NDArray[np.uint16]],
-    th: int = 2,
-    closing_radius: int = 10,
-    opening_radius: int = 10,
-) -> dict[int, CrystalROI]:
-    out: dict[int, CrystalROI] = {}
-    for det, data in sums.items():
-        _mask, croi = find_crystal_range(
-            data > th, closing_radius=closing_radius, opening_radius=opening_radius
-        )
-        out[det] = croi
-    return out
+def compute_masks_rois(sums, opening_radius, closing_radius):
+    masks: dict[tuple[int, int], npt.NDArray[np.integer]] = {}
+    rois: dict[tuple[int, int], CrystalROI] = {}
+    for k, v in sums.items():
+        for th in thresholds:
+            mask, croi = find_crystal_range(
+                v > th, closing_radius=closing_radius, opening_radius=opening_radius
+            )
+            masks[(k, th)] = mask
+            rois[(k, th)] = croi
+
+    return masks, rois
 
 
-# %%
+def update_masks(
+    images: dict[tuple[int, int], AxesImage],
+    rects: dict[tuple[int, int], Rectangle],
+    masks: dict[tuple[int, int], npt.NDArray[np.integer]],
+    rois: dict[tuple[int, int], CrystalROI],
+):
+    for k, mask in masks.items():
+        images[k].set_data(mask)
+    for k, croi in rois.items():
+        rect = rects[k]
+        rect.set_xy((croi.cslc.start, croi.rslc.start))
+        rect.set_width(croi.cslc.stop - croi.cslc.start)
+        rect.set_height(croi.rslc.stop - croi.rslc.start)
 
 
-def extract_khymo(
-    detector_series: npt.NDArray[dt], roi: CrystalROI | None = None
-) -> npt.NDArray[dt]:
-    """
-    Extract the khymograph for a single detector sequence.
+def make_interaction(fig, opening_radius, closing_radius, rects, images):
+    state = {"opening_radius": opening_radius, "closing_radius": closing_radius}
 
-    Parameters
-    ----------
-    detector_series : array
-       Expected order is (time, row, col)
+    def _shared_callback():
+        update_masks(rects, images, *compute_masks_rois(sums, **state))
+        fig.canvas.draw_idle()
 
-    roi: CrystalROI
-       The ROI to consider.  If None, sum whole detector
+    def _update_opening(val):
+        state["opening_radius"] = int(val)
+        _shared_callback()
 
-    Returns
-    -------
+    def _update_closing(val):
+        state["closing_radius"] = int(val)
+        _shared_callback()
 
-    """
-    if roi is None:
-        rslc = cslc = slice(-1)
-    else:
-        rslc = roi.rslc
-        cslc = roi.cslc
+    ax_o, ax_c = fig.subplots(1, 2)
 
-    return cast(npt.NDArray[dt], np.sum(detector_series[:, rslc, cslc], axis=1))
+    s1 = Slider(ax_c, "closing radius", 1, 25, valinit=closing_radius, valstep=1)
+    s2 = Slider(ax_o, "opening_radius", 1, 25, valinit=opening_radius, valstep=1)
+    s1.on_changed(_update_closing)
+    s2.on_changed(_update_opening)
+
+    return s2, s1
 
 
-# %%
-def get_arm_tth(self) -> npt.NDArray[np.float64]:
-    sc = self._mda.scan_config
-    (steps_per_bin,) = sc["MCS prescale"].value
-    (step_size,) = sc["encoder resolution"].value
-
-    bin_size: float = steps_per_bin * step_size
-
-    (Npts,) = sc["NPTS"].value
-    start_tth: float
-    (start_tth,) = sc["start_tth_rbk"].value
-
-    return start_tth + bin_size * np.arange(Npts, dtype=float)
-
-
-def get_monitor(self):
-    return {_.desc: _.data for _ in self._mda.scan.d}
-
+figs, rects, images = make_figure(data_fig, sums, opening_radius, closing_radius)
+keep_alive = make_interaction(input_fig, opening_radius, closing_radius, rects, images)
 
 # %%
-def get_khymograph(
-    raw: RawHRPD11BM, detector: int, roi: CrystalROI | None = None
-) -> tuple[npt.NDArray[np.floating], npt.NDArray[np.uint16]]:
-    """
-    Extract the khymograph for a single detector sequence.
-
-    Parameters
-    ----------
-    raw :
-       Expected order is (time, row, col)
-
-    roi: CrystalROI
-       The ROI to consider.  If None, sum whole detector
-
-    """
-    series = raw.get_detector(detector)
-    print(roi)
-    khymo = extract_khymo(series, roi)
-
-    tth = get_arm_tth(raw)
-
-    return tth, khymo
-
-
-# %%
-
-
-def all_khymographs(
-    raw: RawHRPD11BM, rois: dict[int, CrystalROI]
-) -> dict[int, tuple[npt.NDArray[np.floating], npt.NDArray[np.uint16]]]:
-    return {det: get_khymograph(raw, det, roi) for det, roi in rois.items()}
-
-
-# %%
-
-
-def integrate_simple(tth: npt.NDArray[np.floating], khymo: npt.NDArray[dt]):
-    return tth, khymo.sum(axis=1)
-
-
-# %%
-
-rois2 = compute_rois(sums)
-all_khymos = all_khymographs(t, rois2)
-
-# %%
-
-flats = {det: integrate_simple(tth, khymo) for det, (tth, khymo) in all_khymos.items()}
-
-# %%
-
-
-def estimate_crystal_offsets_ref(
-    raw: RawHRPD11BM,
-    flats: dict[int, tuple[npt.NDArray[np.floating], npt.NDArray[np.uint16]]],
-    ref_indx: int = 0,
-) -> dict[int, float]:
-    sc = raw._mda.scan_config
-    (steps_per_bin,) = sc["MCS prescale"].value
-    (step_size,) = sc["encoder resolution"].value
-
-    bin_size: float = steps_per_bin * step_size
-    ref = flats[ref_indx][1]
-    (Npts,) = ref.shape
-    out: dict[int, float] = {}
-    for det, (_, I) in flats.items():
-        offset = np.argmax(np.correlate(ref, I, mode="full")) - Npts
-        out[det] = offset * bin_size
-
-    return out
-
-
-def estimate_crystal_offsets(
-    raw: RawHRPD11BM,
-    flats: dict[int, tuple[npt.NDArray[np.floating], npt.NDArray[np.uint16]]],
-) -> dict[int, float]:
-    sc = raw._mda.scan_config
-    (steps_per_bin,) = sc["MCS prescale"].value
-    (step_size,) = sc["encoder resolution"].value
-
-    bin_size: float = steps_per_bin * step_size
-
-    out: dict[int, float] = {}
-    iterator = iter(flats.items())
-    det, (_, ref) = next(iterator)
-    (Npts,) = ref.shape
-    out[det] = cum_offset = 0.0
-
-    for det, (_, I) in iterator:
-        offset = np.argmax(np.correlate(ref, I, mode="full")) - Npts
-        cum_offset += offset * bin_size
-        out[det] = cum_offset
-        ref = I
-
-    return out
-
-
-# %%
-
-offsets = estimate_crystal_offsets(t, flats)
-
-# %%
-
-fig, ax = plt.subplots(layout="constrained")
-
-{
-    ax.plot(tth + offsets[d], I + d * 200, label=str(d))[0]
-    for d, (tth, I) in flats.items()
-}
-ax.legend()
-ax.set_xlabel("tth")
-ax.set_ylabel("I")
-
-plt.show()
